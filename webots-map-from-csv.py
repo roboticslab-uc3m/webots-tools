a03eff1afe7e1bccffd9805ab23f465d68d1bdfb
jgvictores <jcgvicto@gmail.com>
2021-01-10 12:40:49 +0100

    initial .wbt contents

--- webots-map-from-csv.py
+++ webots-map-from-csv.py
@@ -5,5 +5,16 @@
 # URL: https://github.com/roboticslab-uc3m/webots-tools
 
 outFile = open('map.wbt', 'w')
-outFile.write("#VRML_SIM R2021a utf8")
+outFile.write('#VRML_SIM R2021a utf8\n\
+WorldInfo {\n\
+  coordinateSystem "NUE"\n\
+}\n\
+Viewpoint {\n\
+  orientation -0.7 0.7 0.2 0.75\n\
+  position 1.2 1.6 2.3\n\
+}\n\
+TexturedBackground {\n\
+}\n\
+TexturedBackgroundLight {\n\
+}\n')
 outFile.close()
