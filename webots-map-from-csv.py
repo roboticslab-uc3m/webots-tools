1bc3d332c2c8fc100b361f45dfda76e038253581
jgvictores <jcgvicto@gmail.com>
2021-01-10 12:34:41 +0100

    init output of map

--- webots-map-from-csv.py
+++ webots-map-from-csv.py
@@ -3,3 +3,7 @@
 # Authors: Juan G Victores
 # CopyPolicy: released under the terms of the LGPLv2.1
 # URL: https://github.com/roboticslab-uc3m/webots-tools
+
+outFile = open('map.wbt', 'w')
+outFile.write("#VRML_SIM R2021a utf8")
+outFile.close()
